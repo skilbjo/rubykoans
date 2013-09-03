995c3e9230e6e0e549cbf6d96590f7f288227467
skilbjo <john.skilbeck@gmail.com>
2013-09-02 17:08:11 -0700

    updates

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError if [a,b,c].min <= 0
+  x, y, z = [a,b,c].sort
+  raise TriangleError if x + y <= z
   if a == b && a == c
   	return :equilateral
   elsif a == b || b == c || a == c
